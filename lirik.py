b44a8458e72a087be504758844206dfc8cc73535
fanz122811 <gntgfanz@gmail.com>
2025-06-03 14:47:20 +0700

    Add files via upload

---
+++ lirik.py
@@ -0,0 +1,21 @@
+import sys
+import time
+
+def jalanin_lirik () 
+        lyrics = [
+                "Everything she never had she's showin' off",
+                    "Drivin' too fast, moon is breakin' through her hair",
+                        "She's headin' for somethin' that she won't forget",
+                            "Havin' no regrets is all that she really wants",
+                                "We're only gettin' older, baby",
+                                    "And I've been thinkin' about it lately",
+                                        "Does it ever drive you crazy",
+                                            "Just how fast the night changes?"
+                                            ]
+
+                                            # Menampilkan lirik satu per satu dengan jeda
+                                            def display_lyrics(lines, delay=2):
+                                                for line in lines:
+                                                        print(line)
+                                                                time.sleep(delay)
+        ]
